51560788a39d6c65c4b19bca170c9becd0394057
Upendra <upendrakuntal1947@gmail.com>
2024-08-10 11:17:57 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                <li>+91-989-898-9890</li>
+                <li>contact@tomato.com</li>
+                </ul>
+
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">
+            Copyright 2024 &copy; Tomato.com - All Right reserved.
+        </p>
+
+        
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+<div className="header">
+    <div className="header-contents">
+        <h2>Order your favourite dishes here</h2>
+        <p>You are few steps away to order your best dishes/</p>
+        <button>View Menu</button>
+    </div>
+</div>
+  )
+}
+
+export default Header
